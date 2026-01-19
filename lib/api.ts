67e44f76b63703fd420ea02daf8136d7fa418a31
Mbogneng-Junior <mbonengj@gmail.com>
2026-01-19 18:24:50 +0100

    deploie6

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,8 @@
 // lib/api.ts
 // Connexion au backend FastAPI pour les cas cliniques
 
-
-const API_BASE_URL = process.env.PYTHON_API_BASE_URL || "http://127.0.0.1:8000/api/v1";
+// Utilisation de 127.0.0.1 pour éviter les problèmes de résolution IPv6/IPv4 sur localhost
+const API_BASE_URL ="http://104.236.244.230:8000/api/v1";
 
 /**
  * Gets authorization headers with token if available
